3d1caf32ae1cbef9ee04e29154e9bbcbe4e37d0e
Nicolas <nico@cooya.fr>
2020-11-26 18:34:44 +0100

    first commit with the main file and the list of names

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # French-Email-Name
+
 Extract the french first name from an email address
+
+## Installation
+
+```bash
+npm i french-email-name
+```
+
+## Usage
+
+```js
+const extractNameFromEmail = require('french-email-name');
+const name = extractNameFromEmail('jeanjacques.goldman@example.com');
+console.log(name); // Jean-Jacques
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const firstNames = require('fs').readFileSync('./first_names.txt').toString().split('\n').map(firstName => {
+	return ({
+		original: firstName,
+		lowercase: firstName.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace('-', '')
+	});
+});
+
+function extractFirstNameFromEmail(email) {
+	const emailSplit = email.replace('-', '').split('@');
+	if(emailSplit[0].match(/^[a-z]\.[a-z]+$/))
+		return null;
+
+	if(emailSplit[0].match(/^[a-z]+\.[a-z]+$/)) {
+		for(let firstName of firstNames) {
+			if(emailSplit[0].match(new RegExp('^' + firstName.lowercase + '\\.')))
+				return firstName.original;
+		}
+	}
+
+	const hardMatches = [];
+	const easyMatches = [];
+
+	for(let firstName of firstNames) {
+		if(emailSplit[0].match(new RegExp(`\\b${firstName.lowercase}\\b`)))
+			hardMatches.push(firstName.original);
+		if(emailSplit[0].includes(firstName.lowercase) && !emailSplit[0].match(new RegExp(`[a-z.]${firstName.lowercase}[a-z.]`)))
+			easyMatches.push(firstName.original);
+	}
+
+	if(hardMatches.length)
+		return hardMatches.filter(x => !!x).sort((a, b) => b.length - a.length)[0];
+	if(easyMatches.length)
+		return easyMatches.filter(x => !!x).sort((a, b) => b.length - a.length)[0];
+
+	return null;
+}
+
+module.exports = extractFirstNameFromEmail;
